dcfc31f3da3d77601cd4f7092d7084b3429561be
Timothé HOFMANN <timothe@logomotion.fr>
2021-10-21 21:18:01 +0200

    I don't know how to describe what I did.

--- lib/managers/HomeMenuStatesManager.dart
+++ lib/managers/HomeMenuStatesManager.dart
@@ -4,11 +4,14 @@ class HomeMenuStatesManager extends ChangeNotifier {
   int index = 1;
 
   HomeMenuStatesManager._private();
-  static final HomeMenuStatesManager _instance = HomeMenuStatesManager._private();
+  static final HomeMenuStatesManager _instance =
+      HomeMenuStatesManager._private();
   static HomeMenuStatesManager get instance => _instance;
 
   changeIndex(int index) {
-    this.index = index;
-    notifyListeners();
+    if (index != this.index) {
+      this.index = index;
+      notifyListeners();
+    }
   }
 }
