e9c4ff82de4186a63e86f2196d6eece2df809f87
Timothé HOFMANN <timothe@logomotion.fr>
2021-10-28 10:19:33 +0200

    DisplayedListManager becomes DisplayedSheetsListManager.

--- lib/components/Pages/Lists/HomePage.dart
+++ lib/components/Pages/Lists/HomePage.dart
@@ -10,6 +10,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Text("Home page des listes.");
+    // return ListDisplayer(height: , displayer: displayer);
   }
 }
