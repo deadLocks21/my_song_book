3fba085a988c1ff2a8c291a91ca5ad884810b19f
Timothé HOFMANN <timothe@logomotion.fr>
2021-11-02 13:30:18 +0100

    Use the new list in SheetsPage.

--- lib/pages/Sheets/SheetsPage.dart
+++ lib/pages/Sheets/SheetsPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_song_book/widgets/ListDisplayer/DisplayableListsStorage.dart';
+import 'package:my_song_book/widgets/ListDisplayer/ListDisplayer.dart';
 
 class SheetsPage extends StatefulWidget {
   SheetsPage({Key? key}) : super(key: key);
@@ -8,10 +10,18 @@ class SheetsPage extends StatefulWidget {
 }
 
 class _SheetsPageState extends State<SheetsPage> {
+  final displayableListsStorage = DisplayableListsStorage.instance;
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('Sheets Page'),
+    return Column(
+      children: [
+        // SearchBar(analyzer: searchASheetManager,),
+        // CategoriesChoice(),
+        ListDisplayer(
+          height: MediaQuery.of(context).size.height * 0.9 - 164,
+          displayedList: displayableListsStorage.list('SHEETS_PAGE'),
+        )
+      ],
     );
   }
 }
